446beafa1f4880b17af706c9c433cde2a603cc95
Amir <aamerhj110@gmail.com>
2026-04-05 22:55:31 +0500

    Update CORS settings to allow all origins for testing

--- hse_api/settings.py
+++ hse_api/settings.py
@@ -167,6 +167,7 @@ SIMPLE_JWT = {
 
 # CORS Configuration (for Flutter frontend)
 CORS_ALLOWED_ORIGINS = [
+    "*",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://amirhj110.github.io",
